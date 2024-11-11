6936a3f0681c6022c94dba4a601c8db081d3ac10
Michael Pigg <mikepigg@highlylogical.com>
2024-11-11 13:27:59 +0000

    Optional http support, clean up logging

--- src/main/scala/ArtifactRepoPlugin.scala
+++ src/main/scala/ArtifactRepoPlugin.scala
@@ -7,8 +7,8 @@ import sbt.{AutoPlugin, Credentials, Def, Keys, MavenRepository}
 import java.io.{File, FileInputStream, FilenameFilter}
 import java.util.Properties
 
-case class ArtifactRepoConfig(host: String, publishRepo: String, pullRepo: String, credentials: Credentials) {
-  def mavenResolver(direction: String): MavenRepository = s"$direction-repo" at s"https://$host/$pullRepo"
+case class ArtifactRepoConfig(host: String, publishRepo: String, pullRepo: String, credentials: Credentials, protocol: String = "https") {
+  def mavenResolver(direction: String): MavenRepository = s"$direction-repo" at s"$protocol://$host/$pullRepo"
 }
 
 object ArtifactRepoPlugin extends AutoPlugin {
@@ -16,17 +16,23 @@ object ArtifactRepoPlugin extends AutoPlugin {
 
   override def requires = sbt.plugins.JvmPlugin
 
-  override def projectSettings: Seq[Def.Setting[_]] = Seq(
-    Keys.credentials ++= artifactRepos.map(_.credentials),
-    Keys.resolvers ++= artifactRepos.map(_.mavenResolver("pull")),
-    Keys.publishTo := artifactRepos.headOption.map(_.mavenResolver("publish"))
-  )
+  override def projectSettings: Seq[Def.Setting[_]] = {
+    val pullRepos = artifactRepos.map(_.mavenResolver("pull"))
+    val publishRepo = artifactRepos.headOption.map(_.mavenResolver("publish"))
+    val credentials = artifactRepos.map(_.credentials)
+    println(s"Adding artifact repos to resolvers: $pullRepos")
+    println(s"Adding artifact repo as publish repo: $publishRepo")
+    Seq(
+      Keys.credentials ++= credentials,
+      Keys.resolvers ++= pullRepos,
+      Keys.publishTo := publishRepo
+    )
+  }
 
 
   lazy val artifactRepos: Seq[ArtifactRepoConfig] = {
     val configFiles = Path.userHome.listFiles(new FilenameFilter {
       override def accept(dir: File, name: String): Boolean = {
-        println(s"check file $name")
         name.endsWith(".artifactrepo")
       }
     })
@@ -39,9 +45,9 @@ object ArtifactRepoPlugin extends AutoPlugin {
   def readRepoConfig(file: File): Either[String, ArtifactRepoConfig]  = {
     val props = new Properties()
     props.load(new FileInputStream(file))
-    val keys = props.keySet().asScala.toSet
+    val keys = props.stringPropertyNames().asScala.toSet
     print(s"Loaded props from $file: $keys")
-    if (keys == Set("realm", "host", "user", "password", "pull-repo", "publish-repo")) {
+    if (Set("realm", "host", "user", "password", "pull-repo", "publish-repo").subsetOf(keys)) {
       Right(ArtifactRepoConfig(
         props.getProperty("host"),
         props.getProperty("publish-repo"),
@@ -51,10 +57,10 @@ object ArtifactRepoPlugin extends AutoPlugin {
           props.getProperty("host"),
           props.getProperty("user"),
           props.getProperty("password")
-        )
+        ),
+        props.getProperty("protocol", "https")
       ))
     } else {
-
       Left(s"Not all properties present")
     }
     }
